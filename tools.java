a8410f0b9e12c92e5b85eff842d25901842cbdef
rao-p <rao_pusarapu@yahoo.com>
2016-10-12 23:35:57 -0500

    Update tools.java

--- tools.java
+++ tools.java
@@ -4,3 +4,8 @@ What are the most sort after tools to implement continuous delivery.
 
 Note: Continuous delivery is mainly dependent on continuous integration 
       and continuous integration solely depends on automation
+      
+      
+Git fast forwards and branch management. In certain situations, Git does a fast forward when you merge branch that is ahead of your 
+  checked-out branch. ... The branch had a single commit (and could have several) before the merge. 
+      The branch is ahead of the master at the point the merge.
